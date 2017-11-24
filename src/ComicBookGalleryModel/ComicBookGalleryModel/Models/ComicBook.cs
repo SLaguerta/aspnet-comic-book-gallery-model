dbd1549eb57e2b12135c7ec1df18ba8814d814f9
Sandra Laguerta <slaguerta@alumni.nd.edu>
2017-11-23 19:18:18 -0600

    Code first workflow; extending Entity data model with one-to-many Series model

---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public Series Series { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public decimal? AverageRating { get; set; }
+
+    }
+}
