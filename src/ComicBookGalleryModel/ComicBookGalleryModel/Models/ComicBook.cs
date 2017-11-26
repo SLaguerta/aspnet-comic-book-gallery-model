393ab647d2a2a125e002807b2895977c58438cf3
Sandra Laguerta <slaguerta@alumni.nd.edu>
2017-11-25 21:29:52 -0600

    Added Artist entity with many-to-many relationship to ComicBook entity

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
@@ -8,6 +8,11 @@ namespace ComicBookGalleryModel.Models
 {
     public class ComicBook
     {
+        public ComicBook()
+        {
+            Artists = new List<Artist>();
+        }
+
         public int Id { get; set; }
         public int SeriesId { get; set; }
         public int IssueNumber { get; set; }
@@ -16,6 +21,7 @@ namespace ComicBookGalleryModel.Models
         public decimal? AverageRating { get; set; }
         
         public Series Series { get; set; }
+        public ICollection<Artist> Artists { get; set; }
 
         public string DisplayText
         {
